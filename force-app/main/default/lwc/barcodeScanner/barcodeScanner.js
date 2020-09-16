8d9cec560d3b7b4d9b3fc27da29b1fdc26270838
sweetykumari15 <official.sweety@gmail.com>
2020-09-16 15:22:20 +0530

    BarcodeScanner

--- force-app/main/default/lwc/barcodeScanner/barcodeScanner.js
+++ force-app/main/default/lwc/barcodeScanner/barcodeScanner.js
@@ -1,14 +1,11 @@
-// barcodeScannerExample.js
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getBarcodeScanner } from 'lightning/mobileCapabilities';
 
-export default class BarcodeScannerExample extends LightningElement {
+export default class BarCodeScannerCmp extends LightningElement {
     myScanner;
     scanButtonDisabled = false;
     scannedBarcode = '';
-
-    // When component is initialized, detect whether to enable Scan button
     connectedCallback() {
         this.myScanner = getBarcodeScanner();
         if (this.myScanner == null || !this.myScanner.isAvailable()) {
@@ -17,41 +14,21 @@ export default class BarcodeScannerExample extends LightningElement {
     }
 
     handleBeginScanClick(event) {
-        // Reset scannedBarcode to empty string before starting new scan
         this.scannedBarcode = '';
-
-        // Make sure BarcodeScanner is available before trying to use it
-        // Note: We _also_ disable the Scan button if there's no BarcodeScanner
         if (this.myScanner != null && this.myScanner.isAvailable()) {
             const scanningOptions = {
                 barcodeTypes: [this.myScanner.barcodeTypes.QR]
             };
-            this.myScanner
-                .beginCapture(scanningOptions)
-                .then((result) => {
-                    console.log(result);
-
-                    // Do something with the barcode scan value:
-                    // - look up a record
-                    // - create or update a record
-                    // - parse data and put values into a form
-                    // - and so on; this is YOUR code
-                    // Here, we just display the scanned value in the UI
+            this.myScanner.beginCapture(scanningOptions).then((result) => {
                     this.scannedBarcode = decodeURIComponent(result.value);
                     this.dispatchEvent(
-                        
                         new ShowToastEvent({
                             title: 'Successful Scan',
                             message: 'Barcode scanned successfully.',
                             variant: 'success'
                         })
                     );
-                })
-                .catch((error) => {
-                    console.error(error);
-
-                    // Handle unexpected errors here
-                    // Inform the user we ran into something unexpected
+                }).catch((error) => {
                     this.dispatchEvent(
                         new ShowToastEvent({
                             title: 'Barcode Scanner Error',
@@ -63,24 +40,10 @@ export default class BarcodeScannerExample extends LightningElement {
                             mode: 'sticky'
                         })
                     );
-                })
-                .finally(() => {
-                    console.log('#finally');
-
-                    // Clean up by ending capture,
-                    // whether we completed successfully or had an error
+                }).finally(() => {
                     this.myScanner.endCapture();
                 });
-        } else {
-            // BarcodeScanner is not available
-            // Not running on hardware with a camera, or some other context issue
-            console.log(
-                'Scan Barcode button should be disabled and unclickable.'
-            );
-            console.log('Somehow it got clicked: ');
-            console.log(event);
-
-            // Let user know they need to use a mobile phone with a camera
+        }else{
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Barcode Scanner Is Not Available',
